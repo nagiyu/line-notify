c791b01349f34d5e831b43f3931bc0b542e447fa
nagiyu <otonagiyu@gmail.com>
2024-07-17 22:27:55 +0900

    chore: Add LineNotify.Tests project, appsettings.json, and related files

---
+++ LineNotify.Tests.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LineNotify.Tests.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
